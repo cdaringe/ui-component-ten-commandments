7cdc14bbd09bfc5bef1bb32b2bf3f9859edfae2b
Christopher Dieringer <cdaringe@users.noreply.github.com>
2025-04-05 11:42:56 -0700

    Update hljs.ts

--- src/util/hljs.ts
+++ src/util/hljs.ts
@@ -1,6 +1,6 @@
 "use client";
 export const init = () =>
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     globalThis as any
   ).hljs.highlightAll();
