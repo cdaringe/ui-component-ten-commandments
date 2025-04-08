6df284ed857f0c2a3fcadceb575c3654ce889917
cdaringe <cdaringe@gmail.com>
2025-04-07 22:27:22 -0700

    chore: next rev

--- src/components/counter-example.tsx
+++ src/components/counter-example.tsx
@@ -5,7 +5,7 @@ export const CounterExample: React.FC<{
 }> = ({ children }) => (
   <details className="counter-example-content mb-2">
     <summary>
-      See a counter example (click to
+      See a naughty example (click to
       expand)
     </summary>
     <div>{children}</div>
